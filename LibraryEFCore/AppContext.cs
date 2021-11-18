fa823e116eb09ccbdf1e4425be3653ef82da4c01
Мирьян Hp <miryan.asmanov@mail.ru>
2021-11-18 12:01:00 +0500

    Добавьте файлы проекта.

---
+++ LibraryEFCore/AppContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryEFCore
+{
+    // класс-модель Авторы
+    public class Authors
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public int YearOfBirth { get; set; }
+        public List<Books> Books { get; set; } = new List<Books>(); // Связь Many-to-many 
+    }
+    // класс-модель Книги
+    public class Books
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int PublicationYear { get; set; }
+        public List<Authors> Authors { get; set; } = new List<Authors>(); // Связь Many-to-many 
+    }
+
+    public class AppContext: DbContext
+    {
+        // Объявляем наборы объектов, через которые идет связь с соответствующей по имени таблицей в БД
+        public DbSet<Authors> Authors { get; set; }
+        public DbSet<Books> Books { get; set; }
+
+        // Конструктор, где пересоздаем БД
+        public AppContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }       
+
+        // Переопределяем метод для передачи строки подключения
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=libraryDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ LibraryEFCore/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LibraryEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Автоматическое закрытие объекта
+            using (AppContext ap = new AppContext())
+            {
+                // Создаем Авторов
+                Authors author1 = new Authors() { FullName = "Пушкин А.С.", YearOfBirth = 1800 };
+                Authors author2 = new Authors() { FullName = "Путин В.В.", YearOfBirth = 1810 };
+                Authors author3 = new Authors() { FullName = "Рошель М.Ю.", YearOfBirth = 1719 };
+                ap.Authors.AddRange(author1, author2, author3);
+
+                // Создаем Книги
+                Books book1 = new Books() { Name = "Как стать программистом гуманитарию", PublicationYear = 1850 };
+                Books book2 = new Books() { Name = "Азы С#", PublicationYear = 1860 };
+                Books book3 = new Books() { Name = "Руководство EFCore", PublicationYear = 1888 };                
+                ap.Books.AddRange(book1, book2);
+
+                // Добавляем Авторам Книги
+                author1.Books.Add(book1);
+                author1.Books.Add(book2);
+                author2.Books.Add(book1);
+                author3.Books.Add(book3);
+
+                // Все изменения отправляем в БД
+                ap.SaveChanges();
+               
+                // Выводим на консоль
+                foreach (var authors in ap.Authors)
+                {
+                    Console.WriteLine(authors.FullName);
+
+                    foreach (var books in authors.Books)
+                    {
+                        Console.WriteLine(books.Name);
+                    }
+                }
+            }
+        }
+    }
+}
