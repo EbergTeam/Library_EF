efce0ab843f9be3cfd13f8f7438c3b20bea86ab1
Мирьян Hp <miryan.asmanov@mail.ru>
2021-11-18 13:50:33 +0500

    Сущности в отдельный файл Реализована миграция

---
+++ LibraryEFCore/Authors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryEFCore
+{
+    // класс-модель Авторы
+    public class Authors
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public int YearOfBirth { get; set; }
+        public List<Books> Books { get; set; } = new List<Books>(); // Связь Many-to-many 
+    }
+}
---
+++ LibraryEFCore/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryEFCore
+{
+    // класс-модель Книги
+    public class Books
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int PublicationYear { get; set; }
+        public List<Authors> Authors { get; set; } = new List<Authors>(); // Связь Many-to-many 
+    }
+}
